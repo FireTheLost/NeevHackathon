fb3610c5ab5f38957db137c1427351bc2196d9fa
Prajwal Reddy <prajwalmsreddy@gmail.com>
2022-03-12 20:26:01 +0530

    Move Scraping Activity To The Server (https://github.com/FireTheLost/NeevHackathonServer)

--- app/src/main/java/com/prajwalmreddy/neevhackathon/MainActivity.kt
+++ app/src/main/java/com/prajwalmreddy/neevhackathon/MainActivity.kt
@@ -4,30 +4,11 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import org.jsoup.Jsoup
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val url = "https://en.wikipedia.org/wiki/Tropical_cyclone"
-
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-            val textview: TextView = findViewById(R.id.textView)
-
-            val thread = Thread {
-                try {
-                    val document = Jsoup.connect(url).get()
-                    textview.text = document.title()
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            }
-
-            thread.start()
-        }
     }
 }
