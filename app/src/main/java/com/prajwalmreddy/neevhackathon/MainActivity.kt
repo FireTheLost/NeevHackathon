07b870318b84464f99a4ddb9ca55667ce4424648
Prajwal Reddy <prajwalmsreddy@gmail.com>
2022-03-12 12:51:39 +0530

    Make A Test App

--- app/src/main/java/com/prajwalmreddy/neevhackathon/MainActivity.kt
+++ app/src/main/java/com/prajwalmreddy/neevhackathon/MainActivity.kt
@@ -2,10 +2,21 @@ package com.prajwalmreddy.neevhackathon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+
+        val randomStrings: Array<String> = arrayOf("Hello There", "'Sup", "Testing", "Checking")
+
+        button.setOnClickListener {
+            val toast = Toast.makeText(this, randomStrings.random(), Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
