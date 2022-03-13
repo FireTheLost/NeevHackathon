0d1d6f50b0f7c9f3809e2dcbafad901e18527809
Prajwal Reddy <prajwalmsreddy@gmail.com>
2022-03-13 09:17:01 +0530

    Create A Simple HTTP Request

--- app/src/main/java/com/prajwalmreddy/neevhackathon/Searcher.java
+++ app/src/main/java/com/prajwalmreddy/neevhackathon/Searcher.java
@@ -1,13 +1,25 @@
 package com.prajwalmreddy.neevhackathon;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Searcher {
     String topic;
+    final String url = "https://localhost:5000"; // Hardcode The Server Url For Now
 
     public Searcher(String topic) {
         this.topic = topic;
     }
 
     public String search() {
+        try {
+            URL url = new URL(this.url);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+        } catch (Exception error) {
+            return error.toString();
+        }
+
         return this.topic;
     }
 }
