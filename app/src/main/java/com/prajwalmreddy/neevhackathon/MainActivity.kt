339f71c56b8f7a065f43887d5cca2eb66ba124ff
Prajwal Reddy <prajwalmsreddy@gmail.com>
2022-03-13 08:57:47 +0530

    Add A Text Input And Start The Searcher Class

--- app/src/main/java/com/prajwalmreddy/neevhackathon/MainActivity.kt
+++ app/src/main/java/com/prajwalmreddy/neevhackathon/MainActivity.kt
@@ -10,5 +10,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val input: TextView = findViewById(R.id.editTextTextPersonName)
+        val textview: TextView = findViewById(R.id.textView)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            // textview.text = input.text
+
+            val searcher = Searcher(input.text.toString())
+            textview.text = searcher.search()
+        }
     }
 }
---
+++ app/src/main/java/com/prajwalmreddy/neevhackathon/Searcher.java
@@ -0,0 +1,13 @@
+package com.prajwalmreddy.neevhackathon;
+
+public class Searcher {
+    String topic;
+
+    public Searcher(String topic) {
+        this.topic = topic;
+    }
+
+    public String search() {
+        return this.topic;
+    }
+}
